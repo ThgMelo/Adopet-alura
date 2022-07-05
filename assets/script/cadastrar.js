8cf647131549fcf629761042ce4d970b82031022
ThgMelo <es.thg.melo@gmail.com>
2022-07-05 08:04:01 -0300

    validação formulário cadastrar

--- assets/script/cadastrar.js
+++ assets/script/cadastrar.js
@@ -1,19 +1,24 @@
-let botao = document.querySelector(".form-botao")
+const botao = document.querySelector(".form-botao");
+const senha1 = document.querySelector("#senha");
+const senha2 = document.querySelector("#conf-senha");
+const mensagem = document.querySelector("#texto-erro");
+botao.disabled = true;
+botao.classList.add("botoes-desabilitado");
 
 function testarSenha(senha1, senha2, mensagem){
 
-    if(senha1.value == senha2.value){
+    if(!(senha1 == senha2)){
         mensagem.innerHTML = "*As senhas não conferem!";
-        mensagem.classList.add("error")
+        mensagem.classList.remove("error");
+        botao.disabled = true;
+        botao.classList.add("botoes-desabilitado");
+    }else{
+        mensagem.classList.add("error");
+        botao.disabled = false;
+        botao.classList.remove("botoes-desabilitado");
     }
 }
 
-botao.addEventListener("click", (event)=>{
-    event.preventDefault();
-    
-    let senha1 = document.querySelector("#senha");
-    let senha2 = document.querySelector("#conf-senha");
-    let mensagem = document.querySelector(".error");
-
-    
+senha2.addEventListener("keyup", ()=>{
+    testarSenha(senha1.value, senha2.value, mensagem);
 })
